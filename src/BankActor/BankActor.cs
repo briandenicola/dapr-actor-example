6a3bf77a0a2aa0a0b8bfa1760b00b9c8295058a5
Alexander Pajer <alexander.pajer@integrations.at>
2023-11-13 19:49:55 +0100

    upgrade

--- src/BankActor/BankActor.cs
+++ src/BankActor/BankActor.cs
@@ -32,7 +32,7 @@ public class BankActor : Actor, IBankActor, IRemindable
         return balance;
     }
 
-    public Task UnRegisterReoccuring(TransferType type) 
+    public Task UnRegisterReoccurring(TransferType type) 
     {
         return type switch 
         {
@@ -42,7 +42,7 @@ public class BankActor : Actor, IBankActor, IRemindable
         };
     }
 
-    public Task RegisterReoccuring(TransferType type, decimal amount)
+    public Task RegisterReoccurring(TransferType type, decimal amount)
     {
         var serializedParams = JsonSerializer.SerializeToUtf8Bytes(amount);
 
@@ -61,7 +61,7 @@ public class BankActor : Actor, IBankActor, IRemindable
         return reminderName switch
         {
             "withdraw" => this.Withdraw(new WithdrawRequest(){ Amount = request}),
-            "deposit" => this.Desposit(new DepositRequest(){ Amount = request}),
+            "deposit" => this.Deposit(new DepositRequest(){ Amount = request}),
             _ => Task.CompletedTask 
         };
     }
@@ -72,7 +72,7 @@ public class BankActor : Actor, IBankActor, IRemindable
         return balance;
     }
 
-    public async Task Desposit(DepositRequest deposit)
+    public async Task Deposit(DepositRequest deposit)
     {
         var balance = await this.StateManager.GetStateAsync<AccountBalance>(AccountId);
         var updated = this.bank.Deposit(balance.Balance, deposit.Amount);
